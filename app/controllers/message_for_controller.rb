26e675df9554e2f7d197e495f3ef16aa1ee460d7
Kosuke Komiya <kosuke.komiya@gmail.com>
2018-11-30 16:45:11 +0900

    Change the order of user's message list.

--- app/controllers/message_for_controller.rb
+++ app/controllers/message_for_controller.rb
@@ -1,5 +1,6 @@
 class MessageForController < ApplicationController
   def show
     @user = User.find_by(username: params[:username])
+    @messages = Message.where(user: @user).order(id: :desc)
   end
 end
